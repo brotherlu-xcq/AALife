2f205c1e7426c6bd7a1cf77df26c7499b41a99cd
mosesc <mosesc@synnex.com>
2018-05-30 18:00:36 +0800

    test jenkins

--- web/src/main/java/com/aalife/web/config/SwaggerConfig.java
+++ web/src/main/java/com/aalife/web/config/SwaggerConfig.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,10 +12,11 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- * Created by mosesc on 05/30/18.
+ *
+ * @author brotherlu
+ * @date 2018-05-30
  */
 @Configuration
 @ConfigurationProperties(prefix = "swagger")
